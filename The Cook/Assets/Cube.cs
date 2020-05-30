d669fc0c10fc33470c7f4612438652c712b432fb
Md Amanulla <mdamanulla44@gmail.com>
2020-05-30 19:27:08 +0600

    TweenManager.cs Added...

---
+++ The Cook/Assets/Cube.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    private void Start()
+    {
+        TweenManager.JumpObject(this.gameObject, new Vector3(5, 1, 0), 5f, 2.0f);
+    }
+}
---
+++ The Cook/Assets/Scripts/TweenManager.cs
@@ -0,0 +1,12 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TweenManager : MonoBehaviour
+{
+    public static void JumpObject(GameObject gameObject, Vector3 endPos, float jumpPower, float durationTime, int numberOfJump = 1)
+    {
+        gameObject.transform.DOLocalJump(endPos, jumpPower, numberOfJump, durationTime);
+    }
+}
